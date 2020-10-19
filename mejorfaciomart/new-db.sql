845e4fbcfc05f389749b9554cef7d7f06d52c24e
dileepkumar0234 <dileepkumarkonda@gmail.com>
2020-10-19 09:01:25 +0530

    Update new-db.sql

--- mejorfaciomart/new-db.sql
+++ mejorfaciomart/new-db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v11.52 (64 bit)
-MySQL - 10.1.32-MariaDB : Database - db_1
+MySQL - 10.1.32-MariaDB : Database - mejorzab_db
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.1.32-MariaDB : Database - db_1
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mejorzab_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `db_1`;
+USE `mejorzab_db`;
 
 /*Table structure for table `cartiformation` */
 
