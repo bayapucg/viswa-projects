295644d059e660cffa2e6a6f74fb2624595b4181
dileepkumar0234 <dileepkumarkonda@gmail.com>
2020-10-19 08:41:26 +0530

    Committed

--- s5/db.sql
+++ s5/db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community v11.52 (64 bit)
-MySQL - 10.1.32-MariaDB : Database - freelstm_db
+MySQL - 10.1.32-MariaDB : Database - shoppgj4_db
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 10.1.32-MariaDB : Database - freelstm_db
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`freelstm_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`shoppgj4_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `freelstm_db`;
+USE `shoppgj4_db`;
 
 /*Table structure for table `cartiformation` */
 
